47d166de6fbb44001da40fe3a612da2ef607da8c
DimVashkov <dimitarvashkov@outlook.com>
2021-12-29 11:28:02 +0200

    add reusable table and segment

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./collections-home.component.css']
 })
 export class CollectionsHomeComponent implements OnInit {
+  data = [
+    { name: 'James', age: 34, job: 'Engineer' },
+    { name: 'John', age: 26, job: 'Doctor' },
+    { name: 'Bob', age:30, job: 'Computer'}
+  ];
+
+  headers = [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age'},
+    { key: 'job', label: 'Job'}
+  ];
 
   constructor() { }
 
